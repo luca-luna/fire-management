2b5a89d16713e43c52d5164c1d6c5de40f45867f
luca-luna <lucaluna@buffalo.edu>
2023-04-28 11:09:46 -0400

    occurrence table from the remaining columns from the occurrence point dataset

---
+++ queries/occurence_point/create_occurrence_point_table.sql
@@ -0,0 +1,38 @@
+-- Table: public.occurence_point
+
+-- DROP TABLE IF EXISTS public.occurence_point;
+
+CREATE TABLE IF NOT EXISTS public.occurrence_point
+(
+    "X" double precision,
+    "Y" double precision,
+    "OBJECTID" integer NOT NULL DEFAULT 1,
+    "GLOBALID" character varying COLLATE pg_catalog."default",
+    "FIREOCCURID" character varying COLLATE pg_catalog."default",
+    "CN" character varying COLLATE pg_catalog."default",
+    "REVDATE" timestamp with time zone,
+    "FIRENAME" character varying COLLATE pg_catalog."default",
+    "COMPLEXNAME" character varying COLLATE pg_catalog."default",
+    "SOFIRENUM" character varying COLLATE pg_catalog."default",
+    "LOCALFIRENUM" character varying COLLATE pg_catalog."default",
+    "REPORTINGUNIT" character varying COLLATE pg_catalog."default",
+    "DISCOVERYDATETIME" timestamp with time zone,
+    "STATCAUSE" character varying COLLATE pg_catalog."default",
+    "COMMENTS" character varying COLLATE pg_catalog."default",
+    "DATASOURCE" character varying COLLATE pg_catalog."default",
+    "FIREOUTDATETIME" time with time zone,
+    "LATDD83" double precision,
+    "LONGDD83" double precision,
+    "FIRETYPECATEGORY" character varying COLLATE pg_catalog."default",
+    "POINTTYPE" character varying COLLATE pg_catalog."default",
+    "PERIMEXISTS" character varying COLLATE pg_catalog."default",
+    "FIRESTATQC" character varying COLLATE pg_catalog."default",
+    "DBSOURCEID" integer NOT NULL,
+    "DBSOURCEDATE" time with time zone NOT NULL,
+    CONSTRAINT occurrence_point_pkey PRIMARY KEY ("OBJECTID")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.occurrence_point
+    OWNER to postgres;
