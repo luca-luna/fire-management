fcbd84e28a374ec1146f51e7a3714c89d4c217f4
luca-luna <lucaluna@buffalo.edu>
2023-05-01 16:51:21 -0400

    put some columns back

--- queries/occurence_point/create_occurrence_point_table.sql
+++ queries/occurence_point/create_occurrence_point_table.sql
@@ -1,6 +1,6 @@
 -- Table: public.occurence_point
 
--- DROP TABLE IF EXISTS public.occurence_point;
+--DROP TABLE IF EXISTS public.occurrence_point;
 
 CREATE TABLE IF NOT EXISTS public.occurrence_point
 (
@@ -17,10 +17,17 @@ CREATE TABLE IF NOT EXISTS public.occurrence_point
     "LOCALFIRENUM" character varying COLLATE pg_catalog."default",
     "REPORTINGUNIT" character varying COLLATE pg_catalog."default",
     "DISCOVERYDATETIME" timestamp with time zone,
+    
+    "TOTALACRES" double precision,
+    
     "STATCAUSE" character varying COLLATE pg_catalog."default",
     "COMMENTS" character varying COLLATE pg_catalog."default",
     "DATASOURCE" character varying COLLATE pg_catalog."default",
     "FIREOUTDATETIME" time with time zone,
+    
+    "UNITDOWNER" character varying COLLATE pg_catalog."default",
+    "UNITDPROTECT" character varying COLLATE pg_catalog."default",
+    
     "LATDD83" double precision,
     "LONGDD83" double precision,
     "FIRETYPECATEGORY" character varying COLLATE pg_catalog."default",
